2c3c3367a100a6ff505ec6f1f7849db5a4e90e2e
majorcek <jasa.stefan@gmail.com>
2017-08-10 10:59:48 +0200

    dodan shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,105 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var = "stevilo.druzin") %>%
-      rename(`Občina` = obcina)
+  
+  # 1. napoved bodočih državnih rekordov
+  output$napoved <- renderPlot({
+    if (input$skupno1 == FALSE){
+      data1 <- filter(nad200_drzave, 
+                    DRŽAVA %in% input$drzava1, 
+                    SKAKALNICA %in% input$skakalnica1)
+                   
+      g1 <- ggplot(data1, aes(x = LETO, y = n, color = DRŽAVA)) +
+        geom_point()
+    
+      g1 + xlim(2011, 2025) +
+        geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
+        xlab("Leto") + ylab("št. poletov čez 220m")
+    } else {
+      data1 <- filter(nad200_drzave, SKAKALNICA %in% input$skakalnica1) 
+      g1 <- ggplot(data1, aes(x = LETO, y = n)) +
+        geom_point()
+      
+      g1 + xlim(2011, 2025) +
+        geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
+        xlab("Leto") + ylab("št. poletov čez 220m")
+    }
   })
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
+  # 2. planica
+  output$ocene <- renderPlot({
+    data2 <- filter(vsi_skoki,
+                    SKAKALNICA == input$skakalnica2,
+                    DRŽAVA %in% input$drzava2,
+                    LETO == input$leto2)
+
+    data2$DOLŽINE_RAZREDI <- round(data2$DOLŽINA/5)*5
+    names(data2)[names(data2) == "OCENE SKUPAJ"] <- "OCENE"
+    data2 <- aggregate(OCENE ~ DOLŽINE_RAZREDI + DRŽAVA , data2, mean)
+    
+    ggplot(data2, aes(x = DOLŽINE_RAZREDI, y = OCENE, color = DRŽAVA)) +
+      geom_line() +
+      #geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
+      geom_point()
+  })
+  
+  # 3. rezultat glede na razmere + zalet
+  output$rezultat <- renderPlot({
+    data3 <- filter(vsi_skoki,
+                    LETO == input$leto3,
+                    SKAKALNICA == input$skakalnica3)
+    data3 <- aggregate(REZULTAT ~ POGOJI_IZRAVNAVA + SKAKALNICA, data3, mean)
+    
+    ggplot(data3,aes(x = POGOJI_IZRAVNAVA, y = REZULTAT, color = SKAKALNICA))+
+      geom_point() +
+      geom_smooth(method = 'lm', fullrange = TRUE, se = FALSE) +
+      geom_line()
+  })
+  
+  # 4. hitrost pri odskoku v odvisnosti od zaleta
+  output$hitrost <- renderPlot({
+    if (input$skupno4 == FALSE){
+      data4 <- filter(vsi_skoki, 
+                      SKAKALNICA == input$skakalnica4,
+                      DRŽAVA %in% input$država4,
+                      LETO == input$leto4)
+      data4 <- aggregate(HITROST ~ DRŽAVA + ZALET, data4, mean)
+      ggplot(data = data4, aes(x = ZALET, y = HITROST, color = DRŽAVA)) +
+        geom_line()+
+        coord_equal()
     } else {
-      t <- obcine
+      data4 <- filter(vsi_skoki, 
+                      SKAKALNICA == input$skakalnica4,
+                      LETO == input$leto4)
+      data4 <- aggregate(HITROST ~ SKAKALNICA + ZALET, data4, mean)
+      ggplot(data = data4, aes(x = ZALET, y = HITROST)) +
+        geom_line()+
+        coord_equal()
     }
-    ggplot(t, aes(x = naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
+  })
+  
+  # 5. zemljevid
+  output$rekordi <- renderPlot({
+    
+    data5 <- filter(pred_for_map1,
+                    metres <= input$rekord,
+                    continent == "Europe")
+    ggplot() +
+      geom_polygon(data = pred_for_map1, aes(x = long, y = lat, group = group), fill = "white", color = "black") +
+      geom_polygon(data = data5,aes(x = long, y = lat, group = group,fill = place), color = "black")+
+      scale_fill_brewer(name = "place", palette = "Spectral", na.value = "grey50")+
+      coord_quickmap(xlim = c(-20,40), ylim = c(35,70))  
   })
 })
+
+
+
+  # output$planica <- renderPlot({
+  # data2 <- filter(vsi_skoki,
+  #                 SKAKALNICA %in% input$skakalnica2,
+  #                 POGOJI == input$pogoji2,
+  #                 ZALET == input$zalet2)
+  # #povprecje dolžin 30 mest, 29. mest ...
+  # data2$POVPRECJE <- summarise(data2,by = DOLŽINA) / length(data2$IME)
+  # g2 <- ggplot(data2,aes(x = LETO, y = DOLŽINA)) + 
+  #   geom_point()
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,94 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
-  titlePanel("Slovenske občine"),
+  titlePanel("Grafični prikaz smučarskih skokov"),
   
   tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
+    tabPanel("napovedi državnih rekordov",
+             sidebarPanel(#'Na spodnjem grafu lahko vidimo predikcijo glede skokov čez 200m na podlagi linearne regresije.' 
+               radioButtons(inputId = 'skakalnica1', 
+                            label = 'Skakalnica:', 
+                            choices = unique(vsi_skoki$SKAKALNICA),
+                            selected = 'Vikersund'),
+               
+               checkboxGroupInput(inputId = 'drzava1',
+                                  label = 'Država:',
+                                # choices = as.character(unique(vsi_skoki$DRŽAVA)),
+                                  choiceValues = list("SVN", "AUT", "DEU","NOR","JPN","POL", "CZE"),
+                                  choiceNames = list("Slovenia","Austria","Germany","Norway","Japan","Poland","Czech Republic"),
+                                  selected = c("AUT","SVN")),
+                                
+               checkboxInput(inputId = "skupno1",
+                             label = "vse države skupaj",
+                             value = FALSE)
+             ),
+             mainPanel(plotOutput("napoved"))),
+
+    
+     tabPanel("sodniške ocene in njihov standardni odklon glede na dolžino skoka",
+              sidebarPanel(
+                #sliderInput('dolžina2', label='Izberi dolžino skoka (zaokroženo na 5m)',
+                #            min=1, max=253.5, step=10, value = 150),
+                sliderInput("leto2", label = "Izberi leto",
+                            min = 2011, max = 2017, value = 2011),
+                checkboxGroupInput(inputId = 'drzava2',
+                                   label = 'Država',
+                                   choiceValues = list("SVN", "AUT", "DEU","NOR","JPN","POL", "CZE"),
+                                   choiceNames = list("Slovenia","Austria","Germany","Norway","Japan","Poland","Czech Republic"),
+                                   selected = c("AUT","SVN")),
+                radioButtons(inputId = 'skakalnica2', 
+                             label = 'Skakalnica', 
+                             choices = unique(vsi_skoki$SKAKALNICA),
+                             selected = 'Vikersund')
+                
+                #checkboxInput(inputId = "skupno2",
+                 #             label = "vse države skupaj",
+                  #            value = FALSE)
+              ),
+              mainPanel(plotOutput("ocene"))),
+    
+    
+     tabPanel("rezultat v seriji glede na skakalnico in pogoje + izravnavo",
+              hr(),
+              sidebarPanel(
+                sliderInput('leto3', label='Izberi leto',
+                            min=2011, max=2017, step=1, value = 2011),
+                checkboxGroupInput(inputId ='skakalnica3', label = 'Izberi skakalnico', 
+                                   choices = unique(vsi_skoki$SKAKALNICA),
+                                   selected = 'Planica')
+              ),
+              mainPanel(plotOutput("rezultat"))),
+     
+     
+     tabPanel("hitrost glede na skakalnico, državo in zaletno mesto",
+              hr(),
+              sidebarPanel(
+                radioButtons(inputId = "skakalnica4",label = 'Izberi skakalnico', choices = list("Planica", "Vikersund", "Obersdorf", "Harrachov", "Kulm"),#unique(vsi_skoki$SKAKALNICA),
+                             selected = 'Planica'),
+                sliderInput(inputId = "leto4", label = "Izberi leto", 
+                            min = 2011, max = 2017, value = 2011),
+                checkboxGroupInput(inputId ='država4', label = 'Izberi državo', 
+                                   choices = unique(vsi_skoki$DRŽAVA),
+                                   selected = 'SVN'),
+                checkboxInput(inputId = "skupno4",
+                              label = "vse države skupaj",
+                              value = FALSE)
+              ),
+              mainPanel(plotOutput("hitrost"))),
+    
+
+    
+    tabPanel("državni rekordi po državah",
+             hr(),
+             sidebarPanel(
+               sliderInput('rekord', label='Izberi dolžino',
+                           min=0, max=253.5, step=0.5, value = 0)
+             ),
+             mainPanel(plotOutput("rekordi")))
+
+  #tags$head(tags$style("#text1{color: red;
+   #                              font-size: 20px;
+    #                   font-style: italic;
+     #                  }"))
+   )
 ))
